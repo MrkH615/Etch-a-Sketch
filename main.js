4b0bb55355e49142d04fc396b6dac906d5908210
Mark Hajnal <mhajnal@sympatico.ca>
2020-01-09 21:51:59 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,4 @@
+const container = document.querySelector('#container');
+const div = document.createElement('div');
+div.style.cssText = 'background: blue; border-style: solid; border-color: red; border-width: 3px';
+container.appendChild(div);
