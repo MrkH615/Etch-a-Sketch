12844457560d6a014abf78065627b8430db18e76
Mark Hajnal <mhajnal@sympatico.ca>
2020-01-20 18:15:52 -0500

    Draws on cells of grid

--- main.js
+++ main.js
@@ -7,8 +7,39 @@ container.style.gridTemplateColumns = `repeat(${columns}, 1fr)`; //works
 
 for (let i=0; i < gridArea; i++) {
   const squareDiv = document.createElement('div');
-  squareDiv.style.cssText = `background: blue; border-style: solid; border-color: red; border-width: 3px`;
+  //squareDiv.style.cssText = `background: blue; border-style: solid; border-color: red; border-width: 3px`;
   squareDiv.classList.add('squareDiv');
   container.appendChild(squareDiv);
+  squareDiv.setAttribute('id',`cell${i}`);
   squareDiv.textContent = `box ${i}`;
 }
+
+
+/*
+document.getElementById('container').addEventListener('mouseover', (event) => {
+  //let coloredBox = event.target.id;
+  console.log(event.target.id);
+  event.target.id.backgroundColor = 'red';  //no color change
+  //event.target.id.classList.add('coloredBox');
+}) ;
+*/
+
+container.addEventListener('mouseover', (event) => {
+  event.target.classList.add('coloredBox');
+});
+
+
+/*
+let draw = document.querySelectorAll('.squareDiv'); //querySelectorAll TypeError: draw.addEventListener is not a function
+container.addEventListener('mouseover', e => {  //mouseover and mouseenter trigger event only once with draw, with container draw.style is undefined
+  //works only on squareDiv 0
+  console.log("Hovered");  //displays message only once with event listener on draw, each time on container
+  draw.forEach(function(e) {
+    draw.style.backgroundColor = 'purple';
+  });
+  //draw.style.backgroundColor = 'purple'; //adds only once
+  //document.getElementsByClassName('squareDiv').style.backgroundColor = 'purple';  //squareDiv not defined
+  //TypeError: document.getElementsByClassName(...).style is undefined
+});
+
+*/
