aafb7d371146f8d3e37a63e8783acca861c6da93
Mark Hajnal <mhajnal@sympatico.ca>
2020-01-26 15:37:12 -0500

    button clears grid/drawing area and makes new one

--- main.js
+++ main.js
@@ -22,15 +22,15 @@ defineDrawingArea(squaresPerSide);
 function makeDrawingArea(gridArea)  {
   for (let i=0; i < gridArea; i++) {
     const squareDiv = document.createElement('div');
-  //squareDiv.style.cssText = `background: blue; border-style: solid; border-color: red; border-width: 3px`;
     squareDiv.classList.add('squareDiv');
     container.appendChild(squareDiv);
     squareDiv.setAttribute('id',`cell${i}`);
   }
 }
 
-function clearDrawingArea(){
+function clearDrawingArea(){  //spaces between drawn sqares if small number of squares
   container.innerHTML = '';
+  container.style.backgroundColor = 'white';
   console.log(`clear drawing area ${squaresPerSide} squares, ${gridArea} area`);
 }
 
