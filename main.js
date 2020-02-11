8d6aa2efeb2297ade79b101b98d7d324e0eb8f48
Mark Hajnal <mhajnal@sympatico.ca>
2020-02-11 17:02:00 -0500

    Makes each square darkeron each mouseenter; doesn't work if Start again clicked

--- main.js
+++ main.js
@@ -28,28 +28,43 @@ function clearDrawingArea(){
 
 makeDrawingArea(gridArea);
 
-let lightness = 100;
+//let lightness = 100;
+/*
 
-function draw() {  //with lightness as arg, first undefined, then NaN
+let lightness = new Array(gridArea).fill(100);
+*/
+
+let lightnessCounter = new Array(gridArea).fill(100);
+
+function draw(...lightnessCounter) {  //with lightness as arg,darkens on pass thru any div in container
   const allSquareDivs = document.querySelectorAll('.squareDiv');
   allSquareDivs.forEach((div) => {
+    //lightness -=10; //after 1st event lightness =-2460
     div.addEventListener('mouseenter', (event) => {
-      console.log(event.target.id);
+      console.log("event.target.id " + event.target.id);  //div id, ex cell5
       console.log(event.target);
-      console.log(lightness);
+      let squareDivNumber = event.target.id.toString().match(/\d+/g);
+      //console.log(lightness);
       //event.target.classList.add('coloredBox');
-      event.target.style.cssText = `background-color:${chooseRandomColor(lightness)}`;
-      lightness -=10;  //decrements with mouseover or mouseover anywhere in container
+      event.target.style.cssText = `background-color: ${chooseRandomColor(squareDivNumber, ...lightnessCounter)}`;
+      //event.target.style.cssText = `background-color:${chooseRandomColor(lightness)}`;
+      lightnessCounter[squareDivNumber] -=10
+      //lightness -=10;  //decrements with mouseover or mouseover anywhere in container
     });
   });
 }
-
+/*
 function chooseRandomColor(lightness) {
   //lightness -=10;  //doesn't change lightness in draw()
   return `hsla(${(Math.random() * 360)}, 100%, ${lightness}%, 1)`;
 }
+*/
 
-draw();
+function chooseRandomColor(squareDivNumber,...lightnessCounter) {
+  return `hsla(${(Math.random() * 360)}, 100%, ${lightnessCounter[squareDivNumber]}%, 1)`;
+}
+
+draw(...lightnessCounter);
 
 /*  works after Start again button clicked
 container.addEventListener('mouseover', (event) => {  //container EventListener colors individual boxes when starting inside container, otherwise colors whole container
