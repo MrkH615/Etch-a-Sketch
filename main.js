95e8faa5d9a2af11ad66ed8d7ae58952f5f19dfa
Mark Hajnal <mhajnal@sympatico.ca>
2022-06-28 14:18:02 -0400

    Clears old squares and draws new grid when 'Start again' button clicked

--- main.js
+++ main.js
@@ -5,13 +5,25 @@ function setSize() {
   return squaresPerSide ** 2;
 }
 
+function drawFirst() {
+  let squaresPerSide = 16;
+  let gridArea = squaresPerSide**2;
+  defineDrawingArea(squaresPerSide);
+  makeDrawingArea(gridArea);
+  draw(gridArea,squaresPerSide);
+  //event listener to call newDraw() here?
+  //newDraw(squaresPerSide);
+  //call newDraw only if "start again" button clicked
+
+}
+
 function defineDrawingArea(squaresPerSide){
   
   container.style.gridTemplateRows = `repeat(${squaresPerSide}, 1fr)`;
   container.style.gridTemplateColumns = `repeat(${squaresPerSide}, 1fr)`;
 }
 
-defineDrawingArea(squaresPerSide=16);
+//defineDrawingArea();
 
 function makeDrawingArea(gridArea)  {
   for (let i=0; i < gridArea; i++) {
@@ -23,19 +35,21 @@ function makeDrawingArea(gridArea)  {
 }
 
 function clearDrawingArea(){  
-  container.innerHTML = '';
+  console.log('inside clearDrawingArea()');
+  container.textContent = '';
+  //container.style.color = '#ebfeff';
   container.style.backgroundColor = 'white';
 }
 
-makeDrawingArea(gridArea=256);
+//makeDrawingArea(gridArea=256);
 
 //let lightnessCounter = new Array(gridArea).fill(90);
 
-function makeLightnessCounter(gridArea) {
+function makeLightnessCounter(gridArea=256) {
    return new Array(gridArea).fill(90);
 }
 
-function draw(gridArea) {  
+function draw(gridArea, squaresPerSide) {  
   const allSquareDivs = document.querySelectorAll('.squareDiv');
   let lightnessCounter = makeLightnessCounter(gridArea);
   allSquareDivs.forEach((div) => {
@@ -45,6 +59,17 @@ function draw(gridArea) {
       lightnessCounter[squareDivNumber] -=10
     });
   });
+  //newDraw(squaresPerSide); constantly called
+  const newSquares = document.querySelector("#newSquares");
+  newSquares.addEventListener('click', newDraw); //not cleared
+  /*newSquares.addEventListener('click', () => {
+    squaresPerSide = prompt(`How many squares do you want on each side? \nCurrently set at ${squaresPerSide} on each side.  `);
+    while (Number.isNaN(Number(squaresPerSide))) {
+      squaresPerSide = prompt(`Please enter a number. \nHow many squares do you want on each side? `);
+    }
+    clearDrawingArea();
+  });*/
+
 }
 
 
@@ -52,25 +77,55 @@ function chooseRandomColor(squareDivNumber,...lightnessCounter) {
   return `hsla(${(Math.random() * 360)}, 100%, ${lightnessCounter[squareDivNumber]}%, 1)`;
 }
 
-draw(gridArea=256);
+//draw(gridArea=256);
 
-let newSquares = document.querySelector("#newSquares");
-newSquares.addEventListener('click', () => {
-  squaresPerSide = prompt(`How many squares do you want on each side? \nCurrently set at ${squaresPerSide} on each side.  `);
-  while (Number.isNaN(Number(squaresPerSide))) {
-    squaresPerSide = prompt(`Please enter a number.  \nHow many squares do you want on each side? `);
-  }
-  squaresPerSide = Math.abs(Math.round(squaresPerSide));
-  clearDrawingArea();
-  defineDrawingArea(squaresPerSide);  
-  gridArea = squaresPerSide * squaresPerSide;
-  lightnessCounter = new Array(gridArea).fill(90);
-  makeDrawingArea(gridArea);
-  draw(...lightnessCounter);
-});
+drawFirst();
+
+function newDraw(squaresPerSide){
+
+  //make separate function with new square that returns squaresPerSide
+ /* let newSquares = document.querySelector("#newSquares");
+  newSquares.addEventListener('click', () => {
+    let squaresPerSide = prompt(`How many squares do you want on each side? \nCurrently set at ${squaresPerSide} on each side.  `);
+    while (Number.isNaN(Number(squaresPerSide))) {
+      squaresPerSide = prompt(`Please enter a number.  \nHow many squares do you want on each side? `);
+    } 
+    squaresPerSide = Math.abs(Math.round(squaresPerSide)); */
+    
+    squaresPerSide = prompt(`How many squares do you want on each side? \nCurrently set at ${squaresPerSide} on each side.  `);
+    while (Number.isNaN(Number(squaresPerSide))) {
+      squaresPerSide = prompt(`Please enter a number.  \nHow many squares do you want on each side? `);
+    } 
+
+    squaresPerSide = Math.abs(Math.round(squaresPerSide));
+    let oldSquaresPerSide = squaresPerSide;
+    squaresPerSide = getNewSquares(oldSquaresPerSide);
+    clearDrawingArea();
+    console.log('back in newDraw()'); //back before "start again" clicked
+    defineDrawingArea(squaresPerSide);
+    gridArea = squaresPerSide ** 2;
+    lightnessCounter = new Array(gridArea).fill(90);
+    makeDrawingArea(gridArea);
+    draw(gridArea, squaresPerSide);
 
 
+//});
+}
 
 
+function getNewSquares(squaresPerSide) {
+  /*const newSquares = document.querySelector("#newSquares");
+  newSquares.addEventListener('click', () => {
+    squaresPerSide = prompt(`How many squares do you want on each side? \nCurrently set at ${squaresPerSide} on each side.  `);
+    while (Number.isNaN(Number(squaresPerSide))) {
+      squaresPerSide = prompt(`Please enter a number. \nHow many squares do you want on each side? `);
+    }
+    clearDrawingArea();
+  }); */
+     
+    squaresPerSide = Math.abs(Math.round(squaresPerSide)); 
+    //clearDrawingArea();
+    return squaresPerSide;
+}
 
-
+//newDraw();
